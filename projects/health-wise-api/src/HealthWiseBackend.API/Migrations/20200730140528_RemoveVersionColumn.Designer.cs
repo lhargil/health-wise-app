690ba061ecf4dff83dafb5370eafa78e94236d64
Lhar Gil <gil_lhar@live.com>
2020-07-30 22:06:09 +0800

    refactor: removed version column

--- projects/health-wise-api/src/HealthWiseBackend.API/Migrations/20200730140528_RemoveVersionColumn.Designer.cs
+++ projects/health-wise-api/src/HealthWiseBackend.API/Migrations/20200730140528_RemoveVersionColumn.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using HealthWiseBackend.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HealthWiseBackend.API.Migrations
 {
     [DbContext(typeof(HealthWiseDbContext))]
-    partial class HealthWiseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200730140528_RemoveVersionColumn")]
+    partial class RemoveVersionColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ projects/health-wise-api/src/HealthWiseBackend.API/Migrations/20200730140528_RemoveVersionColumn.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace HealthWiseBackend.API.Migrations
+{
+    public partial class RemoveVersionColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "BloodPressureReadings");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Version",
+                table: "People",
+                type: "varbinary(4000)",
+                rowVersion: true,
+                nullable: true)
+                .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Version",
+                table: "BloodPressureReadings",
+                type: "varbinary(4000)",
+                rowVersion: true,
+                nullable: true)
+                .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.ComputedColumn);
+        }
+    }
+}
