e4e9ab4690872e1a56cad3c1b5bd1847f29ca3dd
Lhar Gil <devlana@outlook.com>
2020-08-12 21:52:15 +0800

    fix: datetime saving and displaying

--- projects/health-wise-api/src/HealthWiseBackend.API/Dtos/BloodPressureReadingDto.cs
+++ projects/health-wise-api/src/HealthWiseBackend.API/Dtos/BloodPressureReadingDto.cs
@@ -1,4 +1,5 @@
 using System;
+using HealthWiseBackend.API.Entities;
 
 namespace HealthWiseBackend.API.Dtos
 {
@@ -9,5 +10,17 @@ namespace HealthWiseBackend.API.Dtos
     public int Diastole { get; set; }
     public int HeartRate { get; set; }
     public DateTime DateTaken { get; set; }
+
+    public static BloodPressureReadingDto Create(BloodPressureReading bloodPressureReading)
+    {
+      return new BloodPressureReadingDto
+      {
+        Id = bloodPressureReading.Id,
+        Systole = bloodPressureReading.Systole,
+        Diastole = bloodPressureReading.Diastole,
+        HeartRate = bloodPressureReading.HeartRate,
+        DateTaken = new DateTimeOffset(bloodPressureReading.DateTaken).UtcDateTime
+      };
+    }
   }
 }
